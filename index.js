f03da6587f7a62f2fe6b9c2ba5589adbb4785e65
rethchhaiyaa <rethchhaiyaa@gmail.com>
2026-06-04 13:29:49 +0700

    updatecode

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-console.log("Hello World");
-console.log("Hello from ITE");
+// console.log("Hello World");
+// console.log("Hello from ITE");
+
+// let number = 18;
+
+// if(number >= 18)
+// {
+//     console.log("You're able to vote");
+// }
+// else
+// {
+//     console.log("You're not able to vote");
+// }
+
+// for(let i = 1; i <= 10; i++)
+// {
+//     console.log("I love your \n");
+// }
+// function add(a, b)
+// {
+//     return a + b;
+// }
+// console.log("Sum of a + b = "+ add(5, 5));
+// let arr = [1, 2, 4, 5];
+// for(let i = 0; i < 4; i++)
+// {
+//     console.log("Array = " + arr[i]);
+// }
+// const name = "Hello World";
+// console.log("Hello from : " + name);
+// var name1 = "My name is";
+// console.log(name + " " + name1);
+// let number = 10;
+
+// if(number % 2 == 0)
+// {
+//     console.log("This is even number");
+// }
+// else
+// {
+//     console.log("This is odd number");
+// }
+let person = {
+    name : "Reth Chhaiya",
+    age : 19
+};
+console.log(person.name);
+console.log(person.age);
